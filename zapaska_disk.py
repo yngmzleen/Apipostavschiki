4045e1f21cef54b8de66073f316a850e6b3dbe08
yngmzleen <160007357+yngmzleen@users.noreply.github.com>
2024-09-06 22:54:50 +0700

    Update zapaska_disk.py

--- zapaska_disk.py
+++ zapaska_disk.py
@@ -5,6 +5,11 @@ import os
 # URL API для получения данных о дисках
 api_url_disks_1 = "https://ka2.sibzapaska.ru/export.xml"
 api_url_disks_2 = "https://yngmzleen.github.io/drom/products.xml"
+api_url_product = "http://ka2.sibzapaska.ru/API/hs/v1/product"
+
+# Учетные данные для API
+username = "API_client"
+password = "rWp7mFWXRKOq"
 
 # Заголовки для запроса (если необходимо)
 headers = {
@@ -16,10 +21,13 @@ response_1 = requests.get(api_url_disks_1, headers=headers, verify=False)
 response_1.raise_for_status()  # Проверка успешности запроса
 response_2 = requests.get(api_url_disks_2, headers=headers, verify=False)
 response_2.raise_for_status()  # Проверка успешности запроса
+response_product = requests.get(api_url_product, auth=(username, password))
+response_product.raise_for_status()  # Проверка успешности запроса
 
 # Парсинг XML данных
 root_1 = ET.fromstring(response_1.content)
 root_2 = ET.fromstring(response_2.content)
+product_data = response_product.json()
 
 # Создание нового корневого элемента для нового XML файла
 new_root = ET.Element("items")
@@ -59,6 +67,16 @@ for item in root_2.findall('disk'):
             count = count_element.text
             counts_dict[code] = count
 
+# Словарь для хранения оптовых цен из третьей API
+wholesale_prices_dict = {}
+
+# Заполнение словаря оптовых цен из третьей API
+for product in product_data:
+    code = product.get('Код')
+    wholesale_price = product.get('Оптовая_Цена')
+    if code and wholesale_price:
+        wholesale_prices_dict[code] = wholesale_price
+
 # Копирование данных из первой API и добавление цен и остатков из второй API
 for item in root_1.findall('disk'):
     new_item = ET.SubElement(new_root, "item")
@@ -79,6 +97,11 @@ for item in root_1.findall('disk'):
         if code in counts_dict:
             count_element = ET.SubElement(new_item, 'count')
             count_element.text = counts_dict[code]
+    
+    # Добавление оптовой цены, если товар найден в третьей API
+    if code in wholesale_prices_dict:
+        opt_element = ET.SubElement(new_item, 'opt')
+        opt_element.text = wholesale_prices_dict[code]
 
 # Запись данных в новый XML файл
 tree = ET.ElementTree(new_root)
