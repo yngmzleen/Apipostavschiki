49bbab82f0297f7336c855595e5fce4ad587ec32
yngmzleen <160007357+yngmzleen@users.noreply.github.com>
2025-02-01 20:12:31 +0700

    Update zapaska_gruz.py

--- zapaska_gruz.py
+++ zapaska_gruz.py
@@ -2,14 +2,15 @@ import xml.etree.ElementTree as ET
 import requests
 import re
 import os
+import logging
+import warnings
+import json
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
 
 # URL API для получения данных о шинах
-api_url_tyres_2 = "https://yngmzleen.github.io/drom/products.xml"
-api_url_product = "https://ka2.sibzapaska.ru:16500/API/hs/V2/GetTires"
-
-# Учетные данные для API
-username = "API_client"
-password = "rWp7mFWXRKOq"
+api_url = "https://yngmzleen.github.io/drom/tyres.xml"
 
 # Заголовки для запроса (если необходимо)
 headers = {
@@ -17,17 +18,36 @@ headers = {
 }
 
 # Получение данных из API
-response_1 = requests.get(api_url_tyres_1, headers=headers, verify=False)
-response_1.raise_for_status()  # Проверка успешности запроса
-response_2 = requests.get(api_url_tyres_2, headers=headers, verify=False)
-response_2.raise_for_status()  # Проверка успешности запроса
-response_product = requests.get(api_url_product, auth=(username, password))
-response_product.raise_for_status()  # Проверка успешности запроса
+def fetch_data(url):
+    try:
+        response = requests.get(url, headers=headers, verify=False)
+        response.raise_for_status()  # Проверка успешности запроса
+        return response.content
+    except requests.RequestException as e:
+        logging.error(f"Ошибка при запросе данных: {e}")
+        return None
 
 # Парсинг XML данных
-root_1 = ET.fromstring(response_1.content)
-root_2 = ET.fromstring(response_2.content)
-product_data = response_product.json()
+def parse_xml(xml_content):
+    if xml_content:
+        try:
+            return ET.fromstring(xml_content)
+        except ET.ParseError as e:
+            logging.error(f"Ошибка при парсинге XML: {e}")
+            return None
+    return None
+
+# Получение данных из API
+response_content = fetch_data(api_url)
+if not response_content:
+    logging.error("Не удалось получить данные из API")
+    exit(1)
+
+# Парсинг XML
+root = parse_xml(response_content)
+if not root:
+    logging.error("Не удалось распарсить данные из API")
+    exit(1)
 
 # Создание нового корневого элемента для нового XML файла
 new_root = ET.Element("items")
@@ -35,6 +55,8 @@ new_root = ET.Element("items")
 # Поля, которые нужно сохранить и их новые названия
 fields_to_keep = {
     'name': 'name',
+    'retail': 'price',
+    'rest': 'rest',
     'brand': 'brand',
     'model': 'model',
     'code': 'article',
@@ -42,65 +64,26 @@ fields_to_keep = {
     'height': 'height',
     'width': 'width',
     'season': 'season',
-    'vendor_code': 'cae'
+    'vendor_code': 'cae',
+    'img': 'img',
+    'category': 'tyretype'
 }
 
-# Словарь для хранения цен и остатков из второй API
-prices_dict = {}
-counts_dict = {}
-
-# Заполнение словаря цен и остатков из второй API
-for item in root_2.findall('tyres'):
-    nomenclature = item.find('Номенклатура').text
-    price = item.find('Розничая_Цена').text
-    count = item.find('Остаток').text
-    prices_dict[nomenclature] = price
-    counts_dict[nomenclature] = count
-
-# Словарь для хранения оптовых цен из третьей API
-wholesale_prices_dict = {}
-
-# Заполнение словаря оптовых цен из третьей API
-for product in product_data:
-    code = product.get('Код')
-    wholesale_price = product.get('Оптовая_Цена')
-    if code and wholesale_price:
-        wholesale_prices_dict[code] = wholesale_price
-
-# Копирование данных из первой API и добавление цен и остатков из второй API
-for item in root_1.findall('tyres'):
-    load_index = item.find('load_index')
-    if load_index is not None and load_index.text is not None and re.match(r'^\d{2,3}/\d{2,3}$', load_index.text):
+# Копирование данных из API и фильтрация по категории "Грузовая"
+for item in root.findall('tyres'):
+    category = item.find('category')
+    if category is not None and category.text.strip() == "Грузовая":
         new_item = ET.SubElement(new_root, "item")
         
-        for field in fields_to_keep:
+        for field, new_field in fields_to_keep.items():
             element = item.find(field)
-            if element is not None:
-                new_element = ET.SubElement(new_item, fields_to_keep[field])
-                new_element.text = element.text
-        
-        # Добавление цены и остатка, если товар найден во второй API
-        name = item.find('name').text
-        if name in prices_dict:
-            price_element = ET.SubElement(new_item, 'price')
-            price_element.text = prices_dict[name]
-        if name in counts_dict:
-            count_element = ET.SubElement(new_item, 'count')
-            count_element.text = counts_dict[name]
-        
-        # Проверка наличия слова "шип" в поле <name> и добавление поля <spikes>шипы</spikes>
-        if re.search(r'\bшип\b', name, re.IGNORECASE):
-            spikes_element = ET.SubElement(new_item, 'spikes')
-            spikes_element.text = 'шипы'
-        
-        # Добавление оптовой цены, если товар найден в третьей API
-        code = item.find('code').text
-        if code in wholesale_prices_dict:
-            opt_element = ET.SubElement(new_item, 'opt')
-            opt_element.text = wholesale_prices_dict[code]
+            if element is not None and element.text is not None:
+                new_element = ET.SubElement(new_item, new_field)
+                new_element.text = element.text.strip()
 
 # Запись данных в новый XML файл
 tree = ET.ElementTree(new_root)
-tree.write("zapaska_gruz.xml", encoding="utf-8", xml_declaration=True)
+output_file = "zapaska_gruz.xml"
+tree.write(output_file, encoding="utf-8", xml_declaration=True)
 
-print("Новый XML файл для шин успешно создан.")
+logging.info(f"Новый XML файл для легковых шин успешно создан: {output_file}")
